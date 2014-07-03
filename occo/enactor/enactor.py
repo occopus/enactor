b186b91b051ef00494b34b381d62185e78ca60e3
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-07-03 16:58:22 +0200

    Implement Enactor skeleton and some features
    OCD-28

--- occo/enactor/enactor.py
+++ occo/enactor/enactor.py
@@ -2,4 +2,24 @@
 # Copyright (C) 2014 MTA SZTAKI
 #
 
-__all__ = []
+__all__ = ['Enactor']
+
+class Enactor(object):
+    def __init__(self, infrastructure_id, infobroker, infraprocessor, **config):
+        self.infra_id = infrastructure_id
+        self.infobroker = infobroker
+        self.infraprocessor = infraprocessor
+    def get_static_description(self, infra_id):
+        return self.infobroker.get('infrastructure.static_description',
+                                   infra_id=infra_id)
+    def acquire_dynamic_state(self, infra_id):
+        return self.infobroker.get('infrastructure.state', infra_id=infra_id)
+    def calculate_delta(self, static_description, dynamic_state):
+        pass
+    def enact_delta(self, delta):
+        pass
+    def make_a_pass(self):
+        static_description = self.get_static_description(self.infra_id)
+        dynamic_state = self.acquire_dynamic_state(self.infra_id)
+        delta = self.calculate_delta(static_description, dynamic_state)
+        self.enact_delta(delta)
---
+++ tests/unittest/load.py
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2014 MTA SZTAKI
+#
+
+import occo.util.config as cfg
+import occo.enactor as enactor
+import occo.compiler as compiler
+import occo.infobroker as ib
+from functools import wraps
+
+with open('test-config.yaml') as f:
+    config = cfg.DefaultYAMLConfig(f)
+    config.parse_args()
+
+@ib.provider
+class SingletonLocalInfraProcessor(ib.InfoProvider):
+    def __init__(self, static_description):
+        self.static_description = static_description
+        self.process_list = \
+            dict((n, []) for n in static_description.node_lookup.iterkeys())
+
+    @ib.provides('infrastructure.name')
+    def infra_name(self, infra_id, **kwargs):
+        return self.static_description.name
+    
+    @ib.provides('infrastructure.static_description')
+    def infra_descr(self, infra_id, **kwargs):
+        return self.static_description
+
+    @ib.provides('infrastructure.state')
+    def infra_state(self, infra_id, **kwargs):
+        return [(k, len(v)) for (k,v) in self.process_list.iteritems()]
+
+statd = compiler.StaticDescription(config.infrastructure)
+processor = SingletonLocalInfraProcessor(statd)
+e = enactor.Enactor(infrastructure_id=statd.infra_id,
+            infobroker=processor,
+            infraprocessor=processor)
+e.make_a_pass()
