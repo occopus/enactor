bcc3b6d083d69a885799327119d46c106792b7a8
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-07-03 18:18:37 +0200

    OCD-28 Implement very basic Enactor prototype

--- occo/enactor/enactor.py
+++ occo/enactor/enactor.py
@@ -4,6 +4,23 @@
 
 __all__ = ['Enactor']
 
+import itertools as it
+
+def skipimap(fun, iterable):
+    return it.chain.from_iterable(i for i in it.imap(fun, iterable) if i is not None)
+
+# TODO: think this through
+class IPInstruction(dict):
+    def __init__(self, **kwargs):
+        self.update(kwargs)
+class StartNode(IPInstruction):
+    def __init__(self, node):
+        super(StartNode, self).__init__(instruction='start', node=node)
+class StopNode(IPInstruction):
+    def __init__(self, node_id):
+        super(StartNode, self).__init__(instruction='start', node_id=node_id)
+# etc
+
 class Enactor(object):
     def __init__(self, infrastructure_id, infobroker, infraprocessor, **config):
         self.infra_id = infrastructure_id
@@ -15,9 +32,42 @@ class Enactor(object):
     def acquire_dynamic_state(self, infra_id):
         return self.infobroker.get('infrastructure.state', infra_id=infra_id)
     def calculate_delta(self, static_description, dynamic_state):
-        pass
+        infra_id = static_description.infra_id
+
+        def mkdelinst(node):
+            return None
+        def mkcrinst(node):
+            existing = len(dynamic_state[node['name']])
+            #TODO: do default upon loading
+            target = node.get('scaling', dict(min=1, max=1)).get('min', 1)
+            if target <= existing:
+                return []
+            else:
+                return (IPInstruction(instruction='start', node=node)
+                        for i in xrange(target-existing))
+
+        bootstrap_instructions = [[]]
+        if not self.infobroker.get('infrastructure.started',
+                                   infra_id=infra_id):
+            bootstrap_instructions = [[IPInstruction(instruction='create_enviro',
+                                                    infra_id=infra_id)]]
+        del_instructions = \
+            it.chain.from_iterable(
+                skipimap(mkdelinst, nodelist)
+                for nodelist in static_description.topological_order)
+        cr_instructions = it.chain(skipimap(mkcrinst, nodelist)
+                                   for nodelist
+                                   in static_description.topological_order)
+
+        return map(list, it.chain(bootstrap_instructions,
+                                  del_instructions, cr_instructions))
+
     def enact_delta(self, delta):
-        pass
+        for iset in delta:
+            print '[%s]'%(', '.join('{%s -> %s}'%(i['instruction'],
+                                              i.get('node', dict(name=''))['name'])
+                                    for i in iset))
+
     def make_a_pass(self):
         static_description = self.get_static_description(self.infra_id)
         dynamic_state = self.acquire_dynamic_state(self.infra_id)
--- tests/unittest/load.py
+++ tests/unittest/load.py
@@ -6,18 +6,26 @@ import occo.util.config as cfg
 import occo.enactor as enactor
 import occo.compiler as compiler
 import occo.infobroker as ib
+import occo.util.communication as comm
 from functools import wraps
 
 with open('test-config.yaml') as f:
     config = cfg.DefaultYAMLConfig(f)
     config.parse_args()
 
+@comm.register(comm.RPCProducer, 'local')
 @ib.provider
-class SingletonLocalInfraProcessor(ib.InfoProvider):
-    def __init__(self, static_description):
+class SingletonLocalInfraProcessor(ib.InfoProvider,
+                                   comm.RPCProducer):
+    def __init__(self, static_description, **kwargs):
         self.static_description = static_description
         self.process_list = \
             dict((n, []) for n in static_description.node_lookup.iterkeys())
+        self.started = False
+
+    @ib.provides('infrastructure.started')
+    def enviro_created(self, infra_id, **kwargs):
+        return self.started
 
     @ib.provides('infrastructure.name')
     def infra_name(self, infra_id, **kwargs):
@@ -29,11 +37,24 @@ class SingletonLocalInfraProcessor(ib.InfoProvider):
 
     @ib.provides('infrastructure.state')
     def infra_state(self, infra_id, **kwargs):
-        return dict((k, len(v)) for (k,v) in self.process_list.iteritems())
+        return self.process_list
+
+    def start_process(self, msg):
+        pass
+    def stop_process(self, msg):
+        pass
+    def create_environment(self, msg):
+        self.started = True
+    def drop_environment(self, msg):
+        self.started = False
+
+    def push_message(self, message, **kwargs):
+        pass
 
 statd = compiler.StaticDescription(config.infrastructure)
-processor = SingletonLocalInfraProcessor(statd)
+processor = SingletonLocalInfraProcessor(statd, protocol='local')
 e = enactor.Enactor(infrastructure_id=statd.infra_id,
             infobroker=processor,
             infraprocessor=processor)
 e.make_a_pass()
+print processor.get('infrastructure.state', infra_id=statd.infra_id)
