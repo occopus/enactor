228a4fc1c673a74ef991615ba460a84bdb17433d
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-07-04 13:07:05 +0200

    Extract IPInstructions from Enactor
    OCD-28

--- occo/enactor/enactor.py
+++ occo/enactor/enactor.py
@@ -5,20 +5,9 @@
 __all__ = ['Enactor']
 
 import itertools as it
+from instructions import *
 
 flattened = it.chain.from_iterable
-
-# TODO: think this through
-class IPInstruction(dict):
-    def __init__(self, **kwargs):
-        self.update(kwargs)
-class StartNode(IPInstruction):
-    def __init__(self, node):
-        super(StartNode, self).__init__(instruction='start', node=node)
-class StopNode(IPInstruction):
-    def __init__(self, node_id):
-        super(StartNode, self).__init__(instruction='start', node_id=node_id)
-# etc
 
 class Enactor(object):
     def __init__(self, infrastructure_id, infobroker, infraprocessor, **config):
@@ -40,7 +29,7 @@ class Enactor(object):
         return existing[-dropcount:]
     def gen_bootstrap_instructions(self, infra_id):
         if not self.infobroker.get('infrastructure.started', infra_id=infra_id):
-            yield IPInstruction(instruction='create_enviro', infra_id=infra_id)
+            yield IPCreateEnvironment(environment_id=infra_id)
     def calculate_delta(self, static_description, dynamic_state):
         infra_id = static_description.infra_id
 
@@ -53,15 +42,14 @@ class Enactor(object):
         def mkdelinst(node, existing, target):
             exst_count = len(existing)
             if target < exst_count:
-                return (IPInstruction(instruction='drop', node_id=node_id)
+                return (IPDropNode(node_id=node_id)
                         for node_id in self.select_nodes_to_drop(
                                 existing, exst_count - target))
             return []
         def mkcrinst(node, existing, target):
             exst_count = len(existing)
             if target > exst_count:
-                return (IPInstruction(instruction='start', node=node)
-                        for i in xrange(target - exst_count))
+                return (IPStartNode(node=node) for i in xrange(target - exst_count))
             return []
 
         # Create environment if necessary
@@ -75,9 +63,7 @@ class Enactor(object):
 
     def enact_delta(self, delta):
         for iset in delta:
-            print '[%s]'%(', '.join('{%s -> %s}'%(i['instruction'],
-                                              i.get('node', dict(name=''))['name'])
-                                    for i in iset))
+            print '[%s]'%(', '.join('%s'%i for i in iset))
 
     def make_a_pass(self):
         static_description = self.get_static_description(self.infra_id)
