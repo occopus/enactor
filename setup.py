2ea491f76f2e024697aa567a3268898ce485cb4e
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-03-16 17:24:40 +0100

    Update setup stuff for new pip version and add package scripts

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 import setuptools
 from pip.req import parse_requirements
 
-reqs = [ str(i.req) for i in parse_requirements('requirements.txt') ]
-
 setuptools.setup(
     name='OCCO-Enactor',
     version='0.1.0',
@@ -17,5 +15,10 @@ setuptools.setup(
     license='LICENSE.txt',
     description='OCCO Enactor',
     long_description=open('README.txt').read(),
-    install_requires=reqs,
+    install_requires=['python-dateutil',
+                      'argparse',
+                      'PyYAML',
+                      'OCCO-Util',
+                      'OCCO-InfoBroker',
+                      'OCCO-Compiler']
 )
