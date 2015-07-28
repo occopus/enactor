0ac51da0b4a59e523a6c0138f641047062f66ae0
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-07-28 13:34:15 +0000

    Remove stale comments

--- occo/enactor/enactor.py
+++ occo/enactor/enactor.py
@@ -251,10 +251,6 @@ class Enactor(object):
         """
         Make a maintenance pass on the infrastructure.
         """
-        # TODO: if queue is not empty/already working: refuse to do anything
-        # TODO: what to do then? raise exception? log info then NOOP?
-        # TODO: how to check if queue is empty? the enactor does not know about
-        #       the queue (if there is any!)
         # TODO: if there is no queue, how to check whether it is safe to wokr?
         #       probably need to use a Lock()
         static_description = self.get_static_description(self.infra_id)
